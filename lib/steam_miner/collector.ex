6dccaadfe6f2ad3ed4d42494decf7dbe8a9d4423
Raphaël Sfeir <raph.sfeir@gmail.com>
2016-05-18 17:54:03 +0200

    Post message for Steam Collector

--- lib/steam_miner/collector.ex
+++ lib/steam_miner/collector.ex
@@ -1,24 +1,49 @@
 defmodule SteamMiner.Collector do
+  use GenServer
+
+  # Client
+
   @doc """
   Starts new collector.
   """
-  
+
   def start_link do
-    Agent.start_link(fn -> %{} end)
+    GenServer.start_link(__MODULE__, :ok, name: SteamCollector)
   end
 
   @doc """
-  Gets value from collector 
+  looks up the bucket
   """
-  def get(collector, key) do
-    Agent.get(collector, &Map.get(&1, key))
+  def get(name) do
+    GenServer.call(SteamCollector, {:get})
   end
 
   @doc """
-  Put value 
-  """
 
-  def put(collector, key, value) do
-    Agent.update(collector, &Map.put(&1, key, value))
+  """
+  def post(steam_id) do
+    GenServer.cast(SteamCollector, {:post, "454690"})
+  end
+
+  #Server callbacks
+
+  @doc """
+
+  """
+  def init(:ok) do
+    result = %{}
+    {:ok, %{result}}
+  end
+
+  def handle_call({:get}, _from, {names, _} = state) do
+
+  end
+
+  def handle_cast({:post, steam_id}, state) do
+    alias SteamMiner.HttpDownloader
+    alias SteamMiner.HttpParser
+    {:ok, result} = HttpDownloader.get_http_url("http://store.steampowered.com/app/" <> steam_id)
+    {:ok, %{content: content}} = HttpParser.get_element_value(result.body,"div", "apphub_AppName")
+    {:noreply, [content | result]}
   end
 end
